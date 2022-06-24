13f60fd7cd040be449a688ee54b76d8c774d09e7
m-nemati <m.nemati2002@gmail.com>
2022-06-24 10:10:59 +0430

    a few edit done!

--- app/src/main/java/ir/rahmatabad/shahid3768/MainActivity.kt
+++ app/src/main/java/ir/rahmatabad/shahid3768/MainActivity.kt
@@ -1,11 +1,25 @@
 package ir.rahmatabad.shahid3768
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var media1: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        media1 = MediaPlayer.create(this, R.raw.hadadian)
+        media1.start()
     }
-}
+
+    override fun onStop() {
+        media1.stop()
+        super.onStop()
+    }
+
+    }
